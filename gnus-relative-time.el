3cda22cc1a3bcb8d16f5f3d1ad0f6060c84e5bfe
Scott Jaderholm <jaderholm@gmail.com>
2014-03-02 19:34:15 -0800

    Extracted jsj-relative-time function

--- gnus-relative-time.el
+++ gnus-relative-time.el
@@ -9,14 +9,17 @@
 ;;; (setq-default gnus-summary-line-format
 ;;;   "%U%R%z %2t %(%* %s%B%-60=  %-20,20a %u&relative-time;%)\n)")
 
+(defun jsj-relative-time (seconds)
+  (cond ((< seconds 120) (format  "%3d secs" seconds))
+        ((< seconds (* 60 60 2)) (format "%3d mins" (/ seconds 60)))
+        ((< seconds (* 60 60 24 2)) (format "%3d hours" (/ seconds (* 60 60))))
+        ((< seconds (* 60 60 24 14)) (format "%3d days" (/ seconds (* 60 60 24))))
+        ((< seconds (* 60 60 24 61)) (format "%3d weeks" (/ seconds (* 60 60 24 7))))
+        ((< seconds (* 60 60 24 (* 2 365))) (format "%3d months" (/ seconds (* 60 60 24 30))))
+        (t (format "%3d years" (/ seconds (* 60 60 24 365))))))
+
 (defun gnus-user-format-function-relative-time (headers)
   "foo"
   (let ((seconds (time-to-seconds (time-subtract (current-time)
                                                  (gnus-date-get-time (aref headers 3))))))
-    (cond ((< seconds 120) (format  "%3d secs" seconds))
-          ((< seconds (* 60 60 2)) (format "%3d mins" (/ seconds 60)))
-          ((< seconds (* 60 60 24 2)) (format "%3d hours" (/ seconds (* 60 60))))
-          ((< seconds (* 60 60 24 14)) (format "%3d days" (/ seconds (* 60 60 24))))
-          ((< seconds (* 60 60 24 61)) (format "%3d weeks" (/ seconds (* 60 60 24 7))))
-          ((< seconds (* 60 60 24 (* 2 365))) (format "%3d months" (/ seconds (* 60 60 24 30))))
-          (t (format "%3d years" (/ seconds (* 60 60 24 365)))))))
+    (pretty-time seconds)))
